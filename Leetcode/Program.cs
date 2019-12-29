252ac78e34272340a898c73757f5b8d3a95bb20f
Abhishek Shetye <14abhishekshetye10@gmail.com>
2019-12-29 20:21:38 +0530

    initial commit
    -

---
+++ Leetcode/Program.cs
@@ -0,0 +1,17 @@
+﻿using Leetcode.SingleRowKeyboard;
+using System;
+
+namespace Leetcode
+{
+    class Programss
+    {
+        static void Main(string[] args)
+        {
+            var classObj = new Solution();
+
+            
+            Console.ReadKey();
+        }
+    }
+}
+    
---
+++ Leetcode/SingleRowKeyboard/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.SingleRowKeyboard
+{
+    public class Solution
+    {
+        public static int CalculateTime(string keyboard, string word)
+        {
+            Dictionary<char, int> keyboardIndex = new Dictionary<char, int>();
+            for (int i = 0; i < keyboard.Length; i++)
+            {
+                if (!keyboardIndex.ContainsKey(keyboard[i]))
+                {
+                    keyboardIndex.Add(keyboard[i], i);
+                }
+            }
+            int result = 0 ;
+            int lastCharIndex = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                var charIndex = keyboardIndex[word[i]];
+                var res = lastCharIndex - charIndex;
+                result += Math.Abs(res);
+                lastCharIndex = charIndex;
+            }
+
+            return result;
+
+        }
+    }
+}
